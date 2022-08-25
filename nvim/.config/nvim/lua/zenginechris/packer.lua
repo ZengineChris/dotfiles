eaec722ced05834d806b8592f3b817f4ad9e81c2
ZengineChris <zengineChris@gmail.com>
2022-08-25 11:45:29 +0200

    feat: add vim-go plugin

--- nvim/.config/nvim/lua/zenginechris/packer.lua
+++ nvim/.config/nvim/lua/zenginechris/packer.lua
@@ -59,6 +59,7 @@ return require('packer').startup(function(use)
     use("mfussenegger/nvim-dap")
     use("rcarriga/nvim-dap-ui")
     use("theHamsta/nvim-dap-virtual-text")
+    use("fatih/vim-go")
 
     -- Automatically set up your configuration after cloning packer.nvim
     -- Put this at the end after all plugins
