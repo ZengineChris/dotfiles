1c0635e9a23741733f9fc1f238f99b42bd98b086
christian <christian@zengine.dev>
2023-03-19 13:33:53 +0100

    feat: switch to lazy

--- nvim/.config/nvim/lua/zenginechris/init.lua
+++ nvim/.config/nvim/lua/zenginechris/init.lua
@@ -1,4 +1,5 @@
 require("zenginechris.set")
 require("zenginechris.remap")
-require("zenginechris.packer")
+-- require("zenginechris.packer")
+require("zenginechris.lazy")
 
---
+++ nvim/.config/nvim/lua/zenginechris/lazy.lua
@@ -0,0 +1,63 @@
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+
+
+require("lazy").setup({
+    "nvim-lua/plenary.nvim",
+    "folke/which-key.nvim",
+    { "folke/neoconf.nvim", cmd = "Neoconf" },
+    "folke/neodev.nvim",
+    {"rebelot/kanagawa.nvim",
+      config = function()
+            vim.cmd('colorscheme kanagawa')
+        end
+    },
+    {"nvim-treesitter/nvim-treesitter", cmd = "TSUpdate" },
+    "nvim-treesitter/playground",
+    "tpope/vim-fugitive",
+    "mbbill/undotree",
+    "folke/zen-mode.nvim",
+    "nvim-telescope/telescope.nvim",
+    {
+        'nvim-tree/nvim-tree.lua',
+        tag = 'nightly' -- optional, updated every week. (see issue #1193)
+    },
+    {
+        'VonHeikemen/lsp-zero.nvim',
+        dependencies = {
+         -- LSP Support
+            { 'neovim/nvim-lspconfig' },
+            { 'williamboman/mason.nvim' },
+            { 'williamboman/mason-lspconfig.nvim' },
+
+            -- Autocompletion
+            { 'hrsh7th/nvim-cmp' },
+            { 'hrsh7th/cmp-buffer' },
+            { 'hrsh7th/cmp-path' },
+            { 'saadparwaiz1/cmp_luasnip' },
+            { 'hrsh7th/cmp-nvim-lsp' },
+            { 'hrsh7th/cmp-nvim-lua' },
+
+            -- Snippets
+            { 'L3MON4D3/LuaSnip' },
+            { 'rafamadriz/friendly-snippets' },
+        }
+    },
+    "lukas-reineke/indent-blankline.nvim",
+    "theprimeagen/harpoon",
+    {
+        'nvim-lualine/lualine.nvim',
+        dependencies = { 'kyazdani42/nvim-web-devicons' }
+    }
+})
