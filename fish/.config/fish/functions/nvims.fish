5f52faf901d5d2bd39faf443688f5342cf6743b7
ZengineChris <zengineChris@gmail.com>
2023-05-21 14:48:13 +0200

    feat: nvims

---
+++ fish/.config/fish/functions/nvims.fish
@@ -0,0 +1,25 @@
+function nvim-chad
+    env NVIM_APPNAME=NvChad nvim
+end
+
+function nvim-yum
+    env NVIM_APPNAME=nvim-yum nvim
+end
+
+function nvim-astro
+  env NVIM_APPNAME=AstroNvim nvim
+end
+
+function nvims
+    set items nvim-yum NvChad AstroNvim
+    set config (printf "%s\n" $items | fzf --prompt=" Neovim Config  " --height=~50% --layout=reverse --border --exit-0)
+    if [ -z $config ]
+        echo "Nothing selected"
+        return 0
+    else if [ $config = "default" ]
+        set config ""
+    end
+    env NVIM_APPNAME=$config nvim $argv
+end
+
+bind \ca 'nvims\n'```
