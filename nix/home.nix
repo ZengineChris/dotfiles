a452ba0e7e58ac6bd625d0252b776aa5a5c306ef
christian <christian@zengine.dev>
2024-04-14 18:40:57 +0200

    again

--- nix/home.nix
+++ nix/home.nix
@@ -4,72 +4,6 @@
   home.username = "christian";
   home.homeDirectory = "/Users/christian";
 
-
-  home.packages = with pkgs; [
-    deno
-
-
-
-
-    hugo
-
-    btop
-
-
-
-
-
-    git
-    tmux
-    wget
-    curl
-    jq
-    bat
-    fzf
-    eza
-    gnupg
-    ripgrep
-    neofetch
-    fd
-    uutils-coreutils
-    stow
-    # ... add your tools here
-
-
-    # Dev tools 
-    #devenv.packages.aarch64-darwin.devenv
-    neovim
-    starship
-    kitty
-    zsh
-    oh-my-zsh
-    zsh-syntax-highlighting
-    zsh-completions
-    go-task
-    golangci-lint
-
-    # infrastructur
-    podman
-    k3d
-    k9s
-    trivy
-    kustomize
-    kubernetes-helm
-    kubectl
-    natscli
-    nats-server
-
-
-    # languages 
-    go
-    nodejs
-    lua
-    llvm
-    python3
-
-  ];
-
-
   programs.git = {
     enable = true;
     userName = "ZengineChris";
@@ -85,7 +19,7 @@
   # You can update home Manager without changing this value. See
   # the home Manager release notes for a list of state version
   # changes in each release.
-  home.stateVersion = "23.11";
+#  home.stateVersion = "23.11";
 
   # Let home Manager install and manage itself.
   programs.home-manager.enable = true;
