e0cc123f05934c47c32016bff152bdd214e23d58
christian <christian@zengine.dev>
2024-04-14 17:28:59 +0200

    update home.nix

--- nix/home.nix
+++ nix/home.nix
@@ -1,10 +1,42 @@
-{ pkgs, ... }: {
+{ config, pkgs, ... }:
+
+{
+  home.username = "christian";
+  home.homeDirectory = "/Users/christian";
+
+
+  home.packages = with pkgs; [
+    deno
+
+
+
+
+    hugo
+
+    btop
+  ];
+
+
+  programs.git = {
+    enable = true;
+    userName = "ZengineChris";
+    userEmail = "zengineChris@gmail.com";
+  };
+
+
+  # This value determines the home Manager release that your
+  # configuration is compatible with. This helps avoid breakage
+  # when a new home Manager release introduces backwards
+  # incompatible changes.
+  #
+  # You can update home Manager without changing this value. See
+  # the home Manager release notes for a list of state version
+  # changes in each release.
+  home.stateVersion = "23.11";
+
+  # Let home Manager install and manage itself.
   programs.home-manager.enable = true;
 
-  home.packages = [
-    pkgs.ocaml
-    pkgs.deno
 
-  ];
 
 }
