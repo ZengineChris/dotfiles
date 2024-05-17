b2c347b4505a867b7d409ef117ceb044bbd394bc
christian <christian@zengine.dev>
2024-05-17 10:31:38 +0200

    add zsh options

--- modules/home-manager/tools/zsh.nix
+++ modules/home-manager/tools/zsh.nix
@@ -7,12 +7,39 @@
     syntaxHighlighting.enable = true;
     shellAliases = {
       v = "nvim";
+      vim = "nvim";
+      vi = "nvim";
       ll = "eza -lag";
       tree = "eza -T --icons";
       update = "sudo nixos-rebuild switch";
     };
     initExtra = ''
-        export PATH="$HOME/go/bin:$PATH"
+      export PATH="$HOME/go/bin:$PATH"
+
+        bindkey -e
+        bindkey '^j' history-search-forward
+        bindkey '^k' history-search-backward
+
+
+        HISTSIZE=5000
+        HISTFILE=~/.zsh_history
+        SAVEHIST=$HISTSIZE
+        HISTDUP=erase
+
+        setopt appendhistory
+        setopt sharehistory
+        setopt hist_ignore_space
+        setopt hist_ignore_all_dups
+        setopt hist_save_no_dups
+        setopt hist_ignore_dups
+        setopt hist_find_no_dups
+
+
+        zstyle ':completion:*' matcher-list 'm:{a-z}={A-Za-z}'
+
+
+        eval "$(fzf --zsh)"
+
     '';
     oh-my-zsh = {
       enable = true;
