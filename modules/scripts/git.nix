3eae69ca9073041f08121cd5a1b21aa1cc7ee39b
ZengineChris <zengineChris@gmail.com>
2024-07-17 11:03:28 +0200

    fix(scripts): the 'BREAKING CHANGE' is only printed if it is choosen

--- modules/scripts/git.nix
+++ modules/scripts/git.nix
@@ -1,7 +1,31 @@
-{pkgs, ...}:
+{ config, pkgs, ... }:
 
 {
-    pkgs.writeShellScriptBin "git-test" ''
-        echo "hello from here"
-    ''
+  home.packages = [
+    # dependencies 
+    pkgs.gum
+
+    # commit message helper
+    # I have to start with this name to prevent a collission with the oh my zsh 
+    # git plugin
+    (pkgs.writeShellScriptBin "ggc" /*bash*/ ''
+        TYPE=$(gum choose "fix" "feat" "docs" "style" "refactor" "test" "chore" "revert" "ci")
+        BREAKING=$(gum choose "Breaking change" "Normal change")
+        SCOPE=$(gum input --placeholder "scope")
+        if  [[ "$BREAKING" == "Breaking change" ]]; then
+            SUMMARY=$(gum input --value "$TYPE($SCOPE)!: " --placeholder "Summary of this change")
+        else 
+            SUMMARY=$(gum input --value "$TYPE($SCOPE): " --placeholder "Summary of this change")
+        fi
+        
+        DESCRIPTION=$(gum write --placeholder "Details of this change")
+         
+        if  [[ "$BREAKING" == "Breaking change" ]]; then
+            DESCRIPTION="BREAKING CHANGE: $DESCRIPTION"
+        fi
+        # Commit these changes if user confirms
+        gum confirm "Commit changes?" && git commit -m "$SUMMARY" -m "$DESCRIPTION" 
+    '')
+  ];
 }
+
