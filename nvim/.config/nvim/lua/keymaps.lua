a0c2da91b257917d1c50ec44bd5e53fac1c972b6
christian <christian@zengine.dev>
2025-03-30 18:18:01 +0200

    unbind arrow keys in nvim

--- nvim/.config/nvim/lua/keymaps.lua
+++ nvim/.config/nvim/lua/keymaps.lua
@@ -47,3 +47,15 @@ vim.keymap.set("n", "<leader>dss", "<cmd> DapContinue<CR>")
 --- lua
 vim.g.tabby_keybinding_accept = '<Tab>'
 vim.g.tabby_keybinding_trigger_or_dismiss = '<C-\\>'
+
+
+--- unset the arrow keys 
+vim.keymap.set('n', '<Up>', '<Nop>')
+vim.keymap.set('n', '<Down>', '<Nop>')
+vim.keymap.set('n', '<Left>', '<Nop>')
+vim.keymap.set('n', '<Right>', '<Nop>')
+
+vim.keymap.set('i', '<Up>', '<Nop>')
+vim.keymap.set('i', '<Down>', '<Nop>')
+vim.keymap.set('i', '<Left>', '<Nop>')
+vim.keymap.set('i', '<Right>', '<Nop>')
