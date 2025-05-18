f7a230a79df8400a688a43262ac698802bed5d39
christian <christian@zengine.dev>
2025-05-18 17:43:36 +0200

    sign commit

--- home/git.nix
+++ home/git.nix
@@ -55,6 +55,7 @@
         };
         commit.gpgsign = true;
         gpg.format = "ssh";
+        gpg.ssh.allowedSignersFile = "~/.ssh/allowed_signers";
         user.signingkey = "~/.ssh/id_ed25519.pub";
       };
     };
